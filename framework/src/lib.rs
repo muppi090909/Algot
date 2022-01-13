dd38287e72a9d3ea42e6a98a6d628568eb6840dc
Ishaan <whthownothing@gmail.com>
2022-01-13 19:08:26 +0530

    added documentation

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,17 +1,62 @@
+//! A framework for number tokenizers mainly Algot
 pub mod expressions {
     use std::{fmt, ops};
-
+    
+    /// A wrapper around f64 type
+    /// eg
+    /// ```
+    /// use algot_framework::expressions::Decimal;
+    ///
+    /// let decimal = Decimal(1.0);
+    /// assert_eq!(decimal, Decimal(1.0));
+    /// assert_eq!(decimal.0, 1.0f64);
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq)]
     pub struct Decimal(pub f64);
+    /// A wrapper around i64 type
+    /// eg
+    /// ```
+    /// use algot_framework::expressions::Integer;
+    ///
+    /// let integer = Integer(1);
+    /// assert_eq!(integer, Integer(1));
+    /// assert_eq!(integer.0, 1);
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq)]
     pub struct Integer(pub i64);
+    /// A series of possible mathematical operator tokenizers
+    /// eg
+    /// ```
+    /// # use algot_framework::expressions::Op;
+    /// # 
+    /// assert_eq!(Op::new('+'), Op::Add);
+    /// ```
+    /// panic on invalid token
+    /// ```should_panic
+    /// # use algot_framework::expressions::Op;
+    /// # 
+    /// Op::new('_');
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq)]
     #[allow(dead_code)]
-    enum Op {
+    pub enum Op {
         Add,
         Sub,
         Mul,
-        Dev,
+        Div,
+        Mod
+    }
+    impl Op {
+        pub fn new(token: char) -> Self {
+            match token {
+                '+' => Op::Add,
+                '-' => Op::Sub,
+                '*' => Op::Mul,
+                '/' => Op::Div,
+                '%' => Op::Mod,
+                _ => panic!("invalid operator")
+            }
+        }
     }
     impl fmt::Display for Decimal {
         fn fmt(self: &Decimal, f: &mut fmt::Formatter) -> fmt::Result {
